8a5bcee7cde7548feef0263f79d1f81cb4025254
HakimMohammed <mohamed.hakim.dev@gmail.com>
2025-07-13 12:32:18 +0100

    fix list display

--- src/components/grid-listing/items-list/item-component.tsx
+++ src/components/grid-listing/items-list/item-component.tsx
@@ -10,29 +10,34 @@ const ItemComponent = forwardRef<
     resource: Product;
   }
 >(({ className, resource, ...props }, ref) => {
+
+  const isListDisplay = className?.includes("flex");
+
   return (
     <button
       ref={ref}
       type="button"
-      className={`bg-card rounded-lg transition-transform duration-200 ease-in-out hover:scale-105 overflow-hidden border border-border text-start text-left ${className}`}
+      className={`bg-card rounded-lg transition-transform duration-200 ease-in-out hover:scale-105 overflow-hidden border border-border text-start text-left`}
       {...props}
     >
-      <div className="aspect-video bg-muted relative">
-        <Image
-          src={resource.image}
-          alt={resource.label}
-          className="w-full h-full object-cover"
-          fill
-        />
-      </div>
-      <div className="p-4 space-y-3">
-        <div>
-          <h3 className="font-semibold text-lg mb-1">{resource.label}</h3>
-          <p className="text-sm text-muted-foreground mb-2">
-          {resource.category}
-          </p>
-          <Separator className="my-4" />
-          <p className="text-sm text-foreground/80">{resource.description}</p>
+      <div className={`${isListDisplay && "grid grid-cols-3"}`}>
+        <div className="aspect-video bg-muted relative col-span-1">
+          <Image
+            src={resource.image}
+            alt={resource.label}
+            className="w-full h-full object-cover"
+            fill
+          />
+        </div>
+        <div className="p-4 space-y-3 col-span-2">
+          <div>
+            <h3 className="font-semibold text-lg mb-1">{resource.label}</h3>
+            <p className="text-sm text-muted-foreground mb-2">
+              {resource.category}
+            </p>
+            <Separator className="my-4" />
+            <p className="text-sm text-foreground/80">{resource.description}</p>
+          </div>
         </div>
       </div>
     </button>
